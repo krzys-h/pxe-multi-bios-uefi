88e9ad5878d83848c3adb90344dc298c82fa1ac2
krzys-h <krzys_h@interia.pl>
2021-01-15 21:13:09 +0100

    Add script to download binary files

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 Here is a config I use for PXE booting both UEFI and legacy BIOS clients at the same time.
 
 See common.cfg for boot entry configuration, everything else is just machinery to get pxelinux to execute it, loading the plugins from the directory that matches the architecture of the client
-
-TODO: Figure out where I took all of the executables from and add a script that downloads the latest versions from the source, add some documentation
---
+++ download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+VERSION=syslinux-6.03
+
+mkdir -p tftp/{bios,efi32,efi64}
+
+wget https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/$VERSION.tar.gz
+tar xzf $VERSION.tar.gz
+
+cp $VERSION/bios/core/pxelinux.0 tftp/
+cp $VERSION/bios/com32/elflink/ldlinux/ldlinux.c32 tftp/
+cp $VERSION/efi32/com32/elflink/ldlinux/ldlinux.e32 tftp/
+cp $VERSION/efi64/com32/elflink/ldlinux/ldlinux.e64 tftp/
+for platform in bios efi32 efi64; do
+	cp $VERSION/$platform/com32/lib/libcom32.c32 tftp/$platform/
+	cp $VERSION/$platform/com32/libutil/libutil.c32 tftp/$platform/
+	cp $VERSION/$platform/com32/modules/linux.c32 tftp/$platform/
+	cp $VERSION/$platform/com32/menu/menu.c32 tftp/$platform/
+	cp $VERSION/$platform/com32/menu/vesamenu.c32 tftp/$platform/
+done
+
+rm -r $VERSION/ $VERSION.tar.gz
